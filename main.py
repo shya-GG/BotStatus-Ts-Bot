58aa54bc21ea878960a2325d211783dbd8b81fe9
shya-GG <67566315+shya-GG@users.noreply.github.com>
2021-09-16 07:27:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ import datetime
 from pyrogram import Client
 
 user_session_string = os.environ.get("user_session_string")
-bots = [i.strip() for i in os.environ.get("bots").split('')]
+bots = [i.strip() for i in os.environ.get("bots").split(' ')]
 update_channel = os.environ.get("update_channel")
-status_message_ids = [int(i.strip()) for i in os.environ.get("status_message_id").split('')]
+status_message_ids = [int(i.strip()) for i in os.environ.get("status_message_id").split(' ')]
 api_id = int(os.environ.get("api_id"))
 api_hash = os.environ.get("api_hash")
 user_client = Client(session_name=str(user_session_string), api_id=api_id, api_hash=api_hash)
